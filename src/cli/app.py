c53f92743af721541140190a24b2a7966de1e6f1
Bronze Brown <dhamon.nightstar@gmail.com>
2025-08-17 09:30:32 -0400

    Organization and planning

---
+++ src/cli/app.py
@@ -0,0 +1,7 @@
+# this will be the CLI version of the app
+
+# Does appsettings.json exist? if not create it
+# Is default Recording directory defined in appsettings? if not ask user to choose one and define it.
+
+# List recording sessions in recording directory. allow user to select session to transcribe. 
+# each session is a subdirectory in the default recording folder.
---
+++ src/domain/config.py
@@ -0,0 +1,12 @@
+# We are going to have 3 different settings files that we need to be about to create load and save to.
+
+# appsettings.json
+## Default recording directory (each recoding session is in a subdirectory of this)
+
+# gamesettings.json
+## This game settings has multiple game profiles.
+### Game Profile: has a campaign name, a list of players, each player has a Teamspeak display name and character name
+
+# lastsession.json Which tracks the following things.
+## Which recording session was being transcribed
+## Which game profile was being user for transcription
